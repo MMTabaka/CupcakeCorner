3f1f511f1e7cd5a9605be6e72865ec43a7f9c5b4
MMTabaka <miloszmtabaka@gmail.com>
2022-05-14 23:48:30 +0100

    Initial Commit

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Milosz Tabaka on 14/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
